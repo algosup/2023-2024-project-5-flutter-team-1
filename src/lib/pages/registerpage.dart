e81b8c399e930d82aaedbfccb105f3af7caa8e7f
EnzoGuillouche <enzo.guillouche@algosup.com>
2024-05-19 19:30:47 +0200

    Register Update V0.21
    Register Update V0.21 adds the register page's image, at the bottom left of the screen. It pushes the "Next" button a bit on the right.
    Changes also the space between the element, which was necessary to implement the image well.
    Changes the Row of the 1st Text "Register" into a Container to margin the text, which was too high up.

--- src/lib/pages/registerpage.dart
+++ src/lib/pages/registerpage.dart
@@ -22,7 +22,7 @@ class _RegisterPageState extends State<RegisterPage> {
 
   Map<String, String> aText = {
     "fr_FR": "Avant de commencer votre inscription,",
-    "en_US": "Before starting your registration,\nplease read"
+    "en_US": "Before starting your registration,"
   };
 
   Map<String, String> bText = {
@@ -56,14 +56,12 @@ class _RegisterPageState extends State<RegisterPage> {
   };
 
   Map<String, String> iaText = {
-    "fr_FR": "J'autorise l'utilisation de mes\ndonnées pour me mettre en relation\navec des entreprises",
+    "fr_FR":
+        "J'autorise l'utilisation de mes\ndonnées pour me mettre en relation\navec des entreprises",
     "en_US": "I agree that my data be used to\nhelp me find a job"
   };
 
-  Map<String, String> nextText = {
-    "fr_FR": "Suivant",
-    "en_US": "Next"
-  };
+  Map<String, String> nextText = {"fr_FR": "Suivant", "en_US": "Next"};
 
   @override
   Widget build(BuildContext context) {
@@ -71,159 +69,180 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Row(
+            Container(
+              margin: const EdgeInsets.all(50),
+              child: Text(
+                "${registerText[lang]}",
+                style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 30,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Center(
+                child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("${registerText[lang]}",
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 30,
-                      color: Colors.black
-                    )
-                  )
-                )
-              ],
-            ),
-            Center(child: 
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("${aText[lang]}",
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
+                Text("${aText[lang]}",
+                    style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
                       fontWeight: FontWeight.w400,
                       fontSize: 18,
                       color: Colors.black,
-                    )
-                  )),
-                  Text("${bText[lang]}",
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
+                    ))),
+                Text("${bText[lang]}",
+                    style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
                       fontWeight: FontWeight.w400,
                       fontSize: 18,
                       color: Colors.black,
-                    )
-                  )),
-                  Text("${cText[lang]}",
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
+                    ))),
+                Text("${cText[lang]}",
+                    style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
                       fontWeight: FontWeight.w400,
                       fontSize: 18,
                       color: Color.fromRGBO(0, 117, 255, 100),
-                    )
-                  )),
-                  Text("${dText[lang]}",
-                  style: GoogleFonts.poppins(
-                      textStyle: const TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18,
-                        color: Colors.black,
-                      )
-                  )),
-                  GestureDetector(
-                onTap: (){
-                  setState(() {
-                    if(checkOne == false){
-                      checkOne = true;
-                    } else if(checkOne == true){
-                      checkOne = false;
-                    }
-                  });
-                },
-                child:
-                  Row(
+                    ))),
+                Text("${dText[lang]}",
+                    style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                      color: Colors.black,
+                    ))),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (checkOne == false) {
+                        checkOne = true;
+                      } else if (checkOne == true) {
+                        checkOne = false;
+                      }
+                    });
+                  },
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                    Checkbox(value: checkOne, activeColor: Colors.green, tristate:false, onChanged: (newbool) {
-                      setState(() {
-                        checkOne = newbool;
-                      });
-                    }),
-                    Text("${rText[lang]}", style: GoogleFonts.poppins(
-                      textStyle: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w300,
-                        color: checkOne! ? Colors.green : Colors.black,
-                      )
-                    ))
-                  ],),),
-              ],)
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("${yText[lang]}", style: GoogleFonts.poppins(
-                textStyle: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black,
-                )
-              )),
-              Text("${iText[lang]}", style: GoogleFonts.poppins(
-                textStyle: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black,
-                )
-              )),
-              const SizedBox(height: 20,),
-              GestureDetector(
-                onTap: (){
-                  setState(() {
-                    if(checkTwo == false){
-                      checkTwo = true;
-                    } else if(checkTwo == true){
-                      checkTwo = false;
-                    }
-                  });
-                },
-                child:
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                Checkbox(value: checkTwo, activeColor: Colors.green, tristate: false, onChanged: (newbool) {
-                  setState(() {
-                    checkTwo = newbool;
-                  });
-                },),
-                Text("${iaText[lang]}", style: GoogleFonts.poppins(
-                  textStyle: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w300,
-                    color: checkTwo! ? Colors.green : Colors.black,
-                  )
-                )),
-              ],),),
-            ],
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                if(checkOne == true && checkTwo == true){
-                  context.go("/registerPage2");
-                }
-              });
-            },
-            child:
-          Container(
-            width: size.width * 0.6,
-            height: size.width * 0.15,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: (checkOne! && checkTwo!) ? Colors.green : Colors.grey,
+                      Checkbox(
+                          value: checkOne,
+                          activeColor: Colors.green,
+                          tristate: false,
+                          onChanged: (newbool) {
+                            setState(() {
+                              checkOne = newbool;
+                            });
+                          }),
+                      Text("${rText[lang]}",
+                          style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300,
+                            color: checkOne! ? Colors.green : Colors.black,
+                          )))
+                    ],
+                  ),
+                ),
+              ],
+            )),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("${yText[lang]}",
+                    style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                    ))),
+                Text("${iText[lang]}",
+                    style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                    ))),
+                const SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (checkTwo == false) {
+                        checkTwo = true;
+                      } else if (checkTwo == true) {
+                        checkTwo = false;
+                      }
+                    });
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Checkbox(
+                        value: checkTwo,
+                        activeColor: Colors.green,
+                        tristate: false,
+                        onChanged: (newbool) {
+                          setState(() {
+                            checkTwo = newbool;
+                          });
+                        },
+                      ),
+                      Text("${iaText[lang]}",
+                          style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300,
+                            color: checkTwo! ? Colors.green : Colors.black,
+                          ))),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            child: Center(
-              child: Text("${nextText[lang]}", style: GoogleFonts.poppins(
-              textStyle: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w300,
-                color: Colors.white,
-              ))),
+            Row(
+              children: [
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Image.asset(
+                    "assets/images/RegisterImage.png",
+                    width: size.width * 0.4,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (checkOne == true && checkTwo == true) {
+                        context.go("/registerPage2");
+                      }
+                    });
+                  },
+                  child: Container(
+                    width: size.width * 0.5,
+                    height: size.width * 0.15,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color:
+                          (checkOne! && checkTwo!) ? Colors.green : Colors.grey,
+                    ),
+                    child: Center(
+                      child: Text("${nextText[lang]}",
+                          style: GoogleFonts.poppins(
+                              textStyle: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white,
+                          ))),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ))
           ],
         ),
       ),
